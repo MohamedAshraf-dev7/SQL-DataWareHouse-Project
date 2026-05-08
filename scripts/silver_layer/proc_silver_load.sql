cee0677e49b9943b91240ff72e11c1a663bc31dc
Mohamed Ashraf <93211619+MohamedAshraf-dev7@users.noreply.github.com>
2026-05-09 01:59:38 +0300

    Enhance silver layer load procedure with timing logs
    Added timing variables and print statements to track loading times for each section of the silver layer loading process.

--- scripts/silver_layer/proc_silver_load.sql
+++ scripts/silver_layer/proc_silver_load.sql
@@ -2,7 +2,17 @@
 CREATE OR ALTER PROCEDURE silver.load_silver
 AS
 BEGIN
+DECLARE @start_time DATETIME , @end_time DATETIME,@start_total_time DATETIME,@end_total_time DATETIME;
+BEGIN TRY
+    SET @start_total_time = GETDATE();
+     PRINT '=============================';
+	PRINT 'LOADING SILVER LAYER';
+	PRINT '=============================';
 
+	PRINT '-----------------------------';
+	PRINT 'LOADING CRM TABLES'
+	PRINT '-----------------------------';
+    SET @start_time = GETDATE();
 TRUNCATE TABLE silver.crm_cust_info
 INSERT INTO silver.crm_cust_info 
 (cst_id, cst_key, cst_first_name, cst_last_name, cst_matrial_status, cst_gndr, cst_create_date)
@@ -27,7 +37,10 @@ ROW_NUMBER() over(partition by cst_id order by cst_create_date desc) as flag
 from bronze.crm_cust_info 
 where cst_id is not null
 )t where flag = 1 
-
+SET @end_time = GETDATE();
+	PRINT 'Time taken to load : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+PRINT '-----------------------------';
+	SET @start_time = GETDATE();
 TRUNCATE TABLE silver.crm_prd_info
 INSERT INTO silver.crm_prd_info (prd_id, cat_id, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt)
 select
@@ -45,6 +58,11 @@ case  UPPER(TRIM(prd_line))
 prd_start_dt,
 CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) AS datetime)-1 as prd_end_dt
 from bronze.crm_prd_info 
+SET @end_time = GETDATE();
+	PRINT 'Time taken to load : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+PRINT '-----------------------------';
+
+SET @start_time = GETDATE();
 TRUNCATE TABLE silver.crm_sales_details
 INSERT INTO silver.crm_sales_details (sls_ord_num, sls_prd_key, sls_cust_id, sls_order_dt, sls_ship_dt, sls_due_dt, sls_sales, sls_quantity, sls_price)
 
@@ -70,6 +88,12 @@ CASE WHEN sls_price <= 0 or sls_price is null THEN sls_sales / NULLIF(sls_quanti
      ELSE sls_price
      END AS sls_price
 FROM bronze.crm_sales_details 
+SET @end_time = GETDATE();
+	PRINT 'Time taken to load : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+PRINT '-----------------------------';
+
+
+	SET @start_time = GETDATE();
 TRUNCATE TABLE silver.erp_cust_az12
 INSERT INTO silver.erp_cust_az12 (cid, bdate, gen)
 SELECT 
@@ -83,6 +107,13 @@ CASE WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
      ELSE 'N/A'
      END AS gen
 FROM bronze.erp_cust_az12 
+SET @end_time = GETDATE();
+	PRINT 'Time taken to load : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+PRINT '-----------------------------';
+
+
+SET @start_time = GETDATE();
+
 TRUNCATE TABLE silver.erp_loc_a101
 INSERT INTO silver.erp_loc_a101 (cid, cntry)
 select  
@@ -92,6 +123,11 @@ CASE WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
      WHEN TRIM(cntry) IN ('') OR cntry IS NULL THEN 'N/A'
      ELSE  TRIM(cntry) END AS cntry
 from bronze.erp_loc_a101 
+SET @end_time = GETDATE();
+	PRINT 'Time taken to load : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+PRINT '-----------------------------';
+
+SET @start_time = GETDATE();
 TRUNCATE TABLE silver.erp_px_cat_g1v2
 INSERT INTO silver.erp_px_cat_g1v2 (id, cat, subcat, maintenance)
 SELECT 
@@ -100,4 +136,15 @@ cat,
 subcat,
 maintenance
 FROM bronze.erp_px_cat_g1v2
+SET @end_time = GETDATE();
+	PRINT 'Time taken to load : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+PRINT '-----------------------------';
+SET @end_total_time = GETDATE();
+	PRINT 'Time taken to load silver layer: ' + CAST(DATEDIFF(SECOND, @start_total_time, @end_total_time) AS VARCHAR) + ' seconds';
+	
+END TRY
+BEGIN CATCH
+   PRINT 'Error occurred while loading silver layer: ' + ERROR_MESSAGE();
+END CATCH
+
 END
